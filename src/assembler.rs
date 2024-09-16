b206c8b0cfc2f5d14f1b0a909066e371c1d90569
Alex Ripoll <alexripollpiera@gmail.com>
2024-09-16 23:14:47 +0200

    assembler Register parser

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,5 +1,11 @@
 use crate::instruction::Opcode;
-use nom::{bytes::complete::tag, combinator::map, IResult};
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, space0},
+    combinator::{map, map_res},
+    sequence::preceded,
+    IResult,
+};
 
 #[derive(Debug, PartialEq)]
 pub enum Token {
@@ -13,9 +19,20 @@ fn parse_load(input: &str) -> IResult<&str, Token> {
     })(input)
 }
 
+fn parse_register(input: &str) -> IResult<&str, Token> {
+    let (input, _) = space0(input)?; // Handle leading whitespace
+
+    let (input, reg_num) = preceded(
+        tag("$"),
+        map_res(digit1, |digit_str: &str| digit_str.parse::<u8>()),
+    )(input)?;
+
+    Ok((input, Token::Register { idx: reg_num }))
+}
+
 #[cfg(test)]
 mod test {
-    use crate::assembler::{parse_load, Token};
+    use crate::assembler::{parse_load, parse_register, Token};
 
     #[test]
     fn test_parse_opcode_load() {
@@ -28,6 +45,15 @@ mod test {
                     opcode: crate::instruction::Opcode::LOAD,
                 },
             )
+        );
+    }
+
+    #[test]
+    fn test_parse_register() {
+        let input = "$12";
+        assert_eq!(
+            parse_register(input).unwrap(),
+            ("", Token::Register { idx: 12 },)
         );
     }
 }
