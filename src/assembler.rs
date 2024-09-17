e4579b2c7c6a1dfd954b4a43942cdd9badd03728
Alex Ripoll <alexripollpiera@gmail.com>
2024-09-17 10:06:41 +0200

    assembler instruction parser (combinator)

--- src/assembler.rs
+++ src/assembler.rs
@@ -4,6 +4,7 @@ use nom::{
     character::complete::{digit1, space0},
     combinator::{map, map_res},
     sequence::preceded,
+    sequence::tuple,
     IResult,
 };
 
@@ -12,6 +13,34 @@ pub enum Token {
     Opcode { opcode: Opcode },
     Register { idx: u8 },
     Operand { value: i32 },
+}
+
+#[derive(Debug, PartialEq)]
+pub struct AssemblerInstruction {
+    opcode: Token,
+    operand1: Option<Token>,
+    operand2: Option<Token>,
+    operand3: Option<Token>,
+}
+
+pub fn parse_instruction(input: &str) -> IResult<&str, AssemblerInstruction> {
+    let (input, (opcode, _, register, _, operand)) = tuple((
+        parse_load,     // Parse the opcode
+        space0,         // Handle optional spaces
+        parse_register, // Parse the register
+        space0,         // Handle optional spaces
+        parse_operand,  // Parse the integer operand
+    ))(input)?;
+
+    Ok((
+        input,
+        AssemblerInstruction {
+            opcode,
+            operand1: Some(register),
+            operand2: Some(operand),
+            operand3: None, // No third operand in this form
+        },
+    ))
 }
 
 fn parse_load(input: &str) -> IResult<&str, Token> {
@@ -44,7 +73,9 @@ fn parse_operand(input: &str) -> IResult<&str, Token> {
 
 #[cfg(test)]
 mod test {
-    use crate::assembler::{parse_load, parse_operand, parse_register, Token};
+    use crate::assembler::{
+        parse_instruction, parse_load, parse_operand, parse_register, AssemblerInstruction, Token,
+    };
 
     #[test]
     fn test_parse_opcode_load() {
@@ -75,6 +106,25 @@ mod test {
         assert_eq!(
             parse_operand(input).unwrap(),
             ("", Token::Operand { value: 10521 },)
+        );
+    }
+
+    #[test]
+    fn test_parse_instruction() {
+        let parsed = parse_instruction("load $0 #100").unwrap();
+        assert_eq!(
+            parsed,
+            (
+                "",
+                AssemblerInstruction {
+                    opcode: Token::Opcode {
+                        opcode: crate::instruction::Opcode::LOAD
+                    },
+                    operand1: Some(Token::Register { idx: 0 }),
+                    operand2: Some(Token::Operand { value: 100 }),
+                    operand3: None,
+                }
+            )
         );
     }
 }
